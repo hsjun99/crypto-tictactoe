9f2ed19b8ee0babf1bc344bc65d304fa7e723075
Seungjun <hsjun999@gmail.com>
2023-04-02 20:20:32 +0400

    update contract

--- backend/contracts/TicTacToe.sol
+++ backend/contracts/TicTacToe.sol
@@ -27,19 +27,26 @@ contract TicTacToe {
     function play(uint256 gameNum, uint8 x, uint8 y) public {
         require(turn[gameNum] == msg.sender, "It's not your turn");
         require(board[gameNum][x][y] == 0, "This field is already taken");
+        require(status[gameNum] == GameStatus.Playing, "Game is not playing");
 
-        board[gameNum][x][y] = turn[gameNum] == players[gameNum][0] ? 1 : 2;
+        uint8 playerIndex = turn[gameNum] == players[gameNum][0] ? 1 : 2;
 
-        if (checkWin(gameNum, 1)) {
-            winner[gameNum] = players[gameNum][0];
+        board[gameNum][x][y] = playerIndex;
+
+        if (checkWin(gameNum, playerIndex)) {
+            winner[gameNum] = players[gameNum][playerIndex];
             status[gameNum] = GameStatus.Finished;
-        } else if (checkWin(gameNum, 2)) {
-            winner[gameNum] = players[gameNum][1];
+            prize(gameNum);
+            return;
+        }
+
+        turn[gameNum] = turn[gameNum] == players[gameNum][0]
+            ? players[gameNum][1]
+            : players[gameNum][0];
+
+        if (checkTie(gameNum)) {
             status[gameNum] = GameStatus.Finished;
-        } else {
-            turn[gameNum] = turn[gameNum] == players[gameNum][0]
-                ? players[gameNum][1]
-                : players[gameNum][0];
+            tie(gameNum);
         }
     }
 
@@ -47,17 +54,46 @@ contract TicTacToe {
         require(status[gameNum] == GameStatus.Finished, "Game is not finished");
         require(msg.sender == winner[gameNum], "You are not the winner");
 
-        (bool success, ) = msg.sender.call{value: 0.1 ether}("");
+        (bool success, ) = winner[gameNum].call{value: 0.1 ether}("");
         require(success, "Transfer failed.");
     }
 
-    function checkWin(uint256 gameNum, uint8 player) internal view returns (bool) {
+    function tie(uint256 gameNum) internal {
+        require(status[gameNum] == GameStatus.Finished, "Game is not finished");
+
+        (bool success1, ) = players[gameNum][0].call{value: 0.05 ether}("");
+        (bool success2, ) = players[gameNum][1].call{value: 0.05 ether}("");
+        require(success1 && success2, "Transfer failed.");
+    }
+
+    function checkTie(uint256 gameNum) internal view returns (bool) {
+        uint8[3][3] memory gameBoard = board[gameNum];
+        if (
+            gameBoard[0][0] != 0 &&
+            gameBoard[0][1] != 0 &&
+            gameBoard[0][2] != 0 &&
+            gameBoard[1][0] != 0 &&
+            gameBoard[1][1] != 0 &&
+            gameBoard[1][2] != 0 &&
+            gameBoard[2][0] != 0 &&
+            gameBoard[2][1] != 0 &&
+            gameBoard[2][2] != 0
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function checkWin(uint256 gameNum, uint8 playerIndex) internal view returns (bool) {
         uint8[3][3] memory gameBoard = board[gameNum];
 
         // Check rows
         for (uint8 i = 0; i < 3; i++) {
             if (
-                gameBoard[i][0] == player && gameBoard[i][1] == player && gameBoard[i][2] == player
+                gameBoard[i][0] == playerIndex &&
+                gameBoard[i][1] == playerIndex &&
+                gameBoard[i][2] == playerIndex
             ) {
                 return true;
             }
@@ -66,17 +102,27 @@ contract TicTacToe {
         // Check columns
         for (uint8 j = 0; j < 3; j++) {
             if (
-                gameBoard[0][j] == player && gameBoard[1][j] == player && gameBoard[2][j] == player
+                gameBoard[0][j] == playerIndex &&
+                gameBoard[1][j] == playerIndex &&
+                gameBoard[2][j] == playerIndex
             ) {
                 return true;
             }
         }
 
         // Check diagonals
-        if (gameBoard[0][0] == player && gameBoard[1][1] == player && gameBoard[2][2] == player) {
+        if (
+            gameBoard[0][0] == playerIndex &&
+            gameBoard[1][1] == playerIndex &&
+            gameBoard[2][2] == playerIndex
+        ) {
             return true;
         }
-        if (gameBoard[0][2] == player && gameBoard[1][1] == player && gameBoard[2][0] == player) {
+        if (
+            gameBoard[0][2] == playerIndex &&
+            gameBoard[1][1] == playerIndex &&
+            gameBoard[2][0] == playerIndex
+        ) {
             return true;
         }
 
