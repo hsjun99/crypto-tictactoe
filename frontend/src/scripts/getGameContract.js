1975239de58d39d46a9b8cd7199518188f90d0a5
Seungjun <hsjun999@gmail.com>
2023-05-11 12:26:24 +0400

    add backend

--- frontend/src/scripts/getGameContract.js
+++ frontend/src/scripts/getGameContract.js
@@ -1,6 +1,6 @@
 import { ethers } from "ethers"
 import provider from "../utils/provider"
-import TicTacToe from "../../../smartcontract/artifacts/contracts/TicTacToe.sol/TicTacToe.json"
+import TicTacToe from "../../../backend/artifacts/contracts/TicTacToe.sol/TicTacToe.json"
 
 async function getGameContract() {
     const address = "0x4c77E05ab184De5fa83399303b49142050148db3"
